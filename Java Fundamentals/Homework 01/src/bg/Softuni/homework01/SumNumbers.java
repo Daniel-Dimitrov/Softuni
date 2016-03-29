55d04657a058e81955ac93d53e72631a764c6941
Daniel-Dimitrov <ddimit@outlook.com>
2016-03-29 22:45:15 +0300

    forth problem

---
+++ Java Fundamentals/Homework 01/src/bg/Softuni/homework01/SumNumbers.java
@@ -0,0 +1,11 @@
+package bg.Softuni.homework01;
+
+/*
+ Task:
+ Create a Java program that reads a number N from the console and calculates the sum of all numbers from 1 to N (inclusive).
+ */
+public class SumNumbers {
+    private static void main(String[] args){
+        
+    }
+}
