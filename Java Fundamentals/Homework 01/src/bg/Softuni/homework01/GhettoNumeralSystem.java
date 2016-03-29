210967fe990c6f43a52ab839c9f84de2ad96cfa3
Daniel-Dimitrov <ddimit@outlook.com>
2016-03-29 23:29:05 +0300

    Fifth problem

---
+++ Java Fundamentals/Homework 01/src/bg/Softuni/homework01/GhettoNumeralSystem.java
@@ -0,0 +1,21 @@
+package bg.Softuni.homework01;
+
+/*
+    Task:
+Write a program that converts the decimal number system to the ghetto numeral system. In the ghetto, numbers are represented as following:
+0 – Gee
+1 – Bro
+2 – Zuz
+3 – Ma
+4 – Duh
+5  - Yo
+6 – Dis
+7 – Hood
+8 – Jam
+9 – Mack
+ */
+public class GhettoNumeralSystem {
+    public static void main(String[] args){
+        
+    }
+}
