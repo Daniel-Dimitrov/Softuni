04d4c5900132cd06411a770c343de20ce53fa2ee
ddimit@outlook.com <DbkataDe1989>
2016-03-29 21:32:48 +0300

    second problem

--- Java Fundamentals/Homework 01/src/bg/Softuni/homework01/PrintCharacters.java
+++ Java Fundamentals/Homework 01/src/bg/Softuni/homework01/PrintCharacters.java
@@ -1,5 +1,18 @@
 package bg.Softuni.homework01;
 
+/*
+Task:
+Print the characters from ‘a’ to ‘z’ on the console on a single line, separated by a space.
+Use a for-loop. Note: you can directly declare and increment char in the for-loop. for (char c = ‘a’; …)
+ */
+
 public class PrintCharacters {
-        
+    public static void main(String[] args){
+        StringBuilder outputString = new StringBuilder();
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+            outputString.append(letter + " ");
+        }
+        outputString.deleteCharAt(outputString.length()-1);
+        System.out.println(outputString.toString());
+    }
 }
