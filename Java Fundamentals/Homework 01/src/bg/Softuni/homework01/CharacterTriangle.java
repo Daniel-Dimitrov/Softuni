081ab998817971e0860c6ae2eb7a72f9de0af4bc
ddimit@outlook.com <DbkataDe1989>
2016-03-29 21:39:08 +0300

    third problem

---
+++ Java Fundamentals/Homework 01/src/bg/Softuni/homework01/CharacterTriangle.java
@@ -0,0 +1,17 @@
+package bg.Softuni.homework01;
+
+import java.util.Scanner;
+
+/*
+Task:
+Create a triangle of characters, based on input.
+ */
+public class CharacterTriangle {
+    public static void main(String[] args){
+        System.out.println("Please provide number of characters");
+        Scanner inputLine = new Scanner(System.in);
+        Integer charNumber = inputLine.nextInt();
+       
+    }
+}
+
