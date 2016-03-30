da8ae9c0a13ef3bf58db1c2acaea69e7c041d1cd
Daniel-Dimitrov <ddimit@outlook.com>
2016-03-30 08:52:40 +0300

    sixth problem

--- Java Fundamentals/Homework 01/src/bg/Softuni/homework01/AverageNumber.java
+++ Java Fundamentals/Homework 01/src/bg/Softuni/homework01/AverageNumber.java
@@ -1,10 +1,32 @@
 package bg.Softuni.homework01;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Scanner;
+
 /* Task:
 Create a method that finds the average of three numbers. Format the output to two digits after the decimal separator.
  */
 public class AverageNumber {
         public static void main(String[] args){
-            
+            //read input from the console
+            System.out.println("Please provide first number");
+            Scanner inputLine = new Scanner(System.in);
+            BigDecimal a = inputLine.nextBigDecimal();
+            System.out.println("Please provide second number");
+            BigDecimal b = inputLine.nextBigDecimal();
+            System.out.println("Please provide third number");
+            BigDecimal c = inputLine.nextBigDecimal();
+
+            //define average and divider variable
+            BigDecimal average = new BigDecimal(0.00);
+            BigDecimal divider = new BigDecimal(3.00);
+            //calculate the average
+            average = average.add(a);
+            average = average.add(b);
+            average = average.add(c);
+            average = average.divide(divider,2,RoundingMode.FLOOR);
+            //print the result in the console
+            System.out.println(average);
         }
 }
