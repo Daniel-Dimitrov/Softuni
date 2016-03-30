81f7e788f618b2b80b1edd2ccdfebc527d0310d7
Daniel-Dimitrov <ddimit@outlook.com>
2016-03-30 08:39:20 +0300

    Sixth problem

---
+++ Java Fundamentals/Homework 01/src/bg/Softuni/homework01/AverageNumber.java
@@ -0,0 +1,10 @@
+package bg.Softuni.homework01;
+
+/* Task:
+Create a method that finds the average of three numbers. Format the output to two digits after the decimal separator.
+ */
+public class AverageNumber {
+        public static void main(String[] args){
+            
+        }
+}
